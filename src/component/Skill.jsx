f5cf16dcaaefebc3561a38d340c71ace70cb1952
jorge <jorgecarvajal94@hotmail.com>
2024-04-25 15:11:18 -0500

    cambio de carpeta

--- src/component/Skill.jsx
+++ src/component/Skill.jsx
@@ -1,16 +1,7 @@
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import javascript from "../assets/skills/javascript.svg";
-import tailwind from "../assets/skills/tailwind.svg";
-import react from "../assets/skills/react.svg";
-import git from "../assets/skills/git.svg";
-import node from "../assets/skills/nodejs.svg";
-import html from "../assets/skills/html.svg";
-import bootstrap from "../assets/skills/bootstrap-4-logo-svgrepo-com.svg";
-import css3 from "../assets/skills/css3-svgrepo-com.svg";
-import nextjs from "../assets/skills/nextjs-svgrepo-com.svg";
-import typescript from "../assets/skills/typescript-official-svgrepo-com.svg";
+
 import hr from "../assets/curve-hr.svg";
 import SkillCard from "./SkillCard";
 
@@ -33,16 +24,16 @@ export default function Skills() {
 
       <div className="mt-4">
         <Slider {...settings}>
-          <SkillCard name="React" img={react} />
-          <SkillCard name="Next" img={nextjs} />
-          <SkillCard name="JavaScript" img={javascript} />
-          <SkillCard name="TypeScript" img={typescript} />
-          <SkillCard name="Tailwind" img={tailwind} />
-          <SkillCard name="Bootstrap" img={bootstrap} />
-          <SkillCard name="Git" img={git} />
-          <SkillCard name="Node" img={node} />
-          <SkillCard name="Css3" img={css3} />
-          <SkillCard name="HTML5" img={html} />
+          <SkillCard name="React" img={"skills/react.svg"} />
+          <SkillCard name="Next" img={"skills/nextjs.svg"} />
+          <SkillCard name="JavaScript" img={"skills/javascript.svg"} />
+          <SkillCard name="TypeScript" img={"skills/typescript.svg"} />
+          <SkillCard name="Tailwind" img={"skills/tailwind.svg"} />
+          <SkillCard name="Bootstrap" img={"skills/bootstrap.svg"} />
+          <SkillCard name="Git" img={"skills/git.svg"} />
+          <SkillCard name="Node" img={"skills/node.svg"} />
+          <SkillCard name="Css3" img={"skills/css3.svg"} />
+          <SkillCard name="HTML5" img={"skills/html.svg"} />
         </Slider>
       </div>
       <img src={hr} className="w-full mt-8 md:h-3" alt="hr" />
