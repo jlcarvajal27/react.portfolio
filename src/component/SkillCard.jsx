2f51d8f59c9764ac19cd773f9dcd4723d1ddd27d
jorge <jorgecarvajal94@hotmail.com>
2024-04-25 14:32:31 -0500

    correcion

--- src/component/SkillCard.jsx
+++ src/component/SkillCard.jsx
@@ -1,4 +1,6 @@
-export default function SkillCard(props) {
+import React from "react";
+
+export default function SkillCard({ img, name }) {
   return (
     <div
       data-aos="fade-up"
@@ -6,9 +8,9 @@ export default function SkillCard(props) {
       data-aos-offset="100"
       className="w-11/12  p-4 text-center justify-center border-2 border-gray-800 bg-dark-200 rounded-md flex flex-col h-48"
     >
-      <img src={props.img} className="w-20 max-h-20 mx-auto" alt={props.name} />
+      <img src={img} className="w-20 max-h-20 mx-auto" alt={name} />
       <div className="mt-2">
-        <h1 className="font-bold md:text-xl">{props.name}</h1>
+        <h1 className="font-bold md:text-xl">{name}</h1>
       </div>
     </div>
   );
